68499ad22fb8a323c7be5cb182fb40bf511e92e2
Eleonora-f <eleonora.falconi@studio.unibo.it>
2025-03-24 11:21:06 +0100

    ex1 done

--- src/test/scala/tasks/adts/ComplexNumberTests.scala
+++ src/test/scala/tasks/adts/ComplexNumberTests.scala
@@ -2,7 +2,7 @@ package tasks.adts
 
 import org.junit.*
 import org.junit.Assert.*
-import tasks.adts.Ex1ComplexNumbers.*
+import tasks.adts.Ex1ComplexNumber.*
 
 /* Tests should be clear, but note they are expressed independently of the 
    specific implementation
